af30f07a69de2640752c5c742df2e81dc65e7e48
Mikkel Hansen <themik1337@gmail.com>
2024-05-21 21:29:26 +0200

    Added type for exclude files

--- main.go
+++ main.go
@@ -5,17 +5,33 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"os"
 	"retailer/cmd"
 )
 
+type excludes []string
+
 func main() {
 
+	var excludedFiles excludes
+
 	maxTails := flag.Int("m", 10, "Max tails running at once")
+	flag.Var(&excludedFiles, "exclude","File to be excluded")
 	flag.Parse()
 	if len(flag.Args()) < 1 {
 		println("Must provide path to monitor")
 		os.Exit(1)
 	}
+fmt.Println("Excludes: ", excludedFiles)
 	cmd.MonitorDir(flag.Arg(0), *maxTails)
+}
+
+func (n *excludes) Set(value string) error {
+    *n = append(*n, value)
+    return nil
+}
+
+func (n *excludes) String() string {
+    return fmt.Sprintf("%s", *n)
 }
